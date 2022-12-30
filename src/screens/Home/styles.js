161c3626dda4ab847ec0e4129fa8acb3f3756ff2
Hafif <hafifimammuddyn@gmail.com>
2022-12-30 19:52:02 +0700

    💄 Added styles for home screen

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../constants';
+
+export default StyleSheet.create({
+  banner: {
+    borderRadius: 30,
+    width: '100%',
+  },
+  bannerWrapper: {
+    alignItems: 'center',
+    marginTop: 49,
+    marginHorizontal: '8%',
+  },
+  button: {
+    backgroundColor: COLORS.orange,
+    borderRadius: 30,
+    paddingVertical: 17,
+    paddingHorizontal: 40,
+    position: 'absolute',
+    bottom: -30,
+  },
+  buttonLabel: {
+    color: COLORS.white,
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  container: {
+    backgroundColor: COLORS.white,
+    flex: 1,
+  },
+  subTitle: {
+    color: COLORS.gray,
+    fontFamily: 'Poppins-Regular',
+    fontWeight: '400',
+    fontSize: 18,
+  },
+  subTitleWrapper: {
+    alignItems: 'center',
+  },
+  titleBrand: {
+    color: COLORS.primary,
+    fontFamily: 'Poppins-Bold',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  titleWrapper: {
+    alignItems: 'center',
+    marginBottom: 16,
+    marginTop: 86,
+  },
+});
