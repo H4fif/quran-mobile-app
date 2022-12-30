a4e0199a80688673572d9ce8cc3d0bb9f850fccb
Hafif <hafifimammuddyn@gmail.com>
2022-12-30 19:47:21 +0700

    ✨ Added exported images ready to use

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+export const SplashScreen = require('../assets/images/SplashScreen.png');
+export const QuranHeader = require('../assets/images/quran-header.png');
+export const PlayerBackground = require('../assets/images/player-background.png');
+export const PlayerBackgroundShadow = require('../assets/images/player-background-shadow.png');
+export const Star = require('../assets/images/star.png');
+
+export default {
+  PlayerBackground,
+  PlayerBackgroundShadow,
+  SplashScreen,
+  Star,
+  QuranHeader,
+};
