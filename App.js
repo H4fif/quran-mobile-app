9c7ae7f6298fe20cdab816ce23ed59c963fcc94d
Hafif <hafifimammuddyn@gmail.com>
2022-12-31 16:02:47 +0700

    ♻️ Implement routes

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import Home from './src/screens/Home';
-import Dashboard from './src/screens/Dashboard';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
+import Router from './src/router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-      </Stack.Navigator>
+      <Router />
     </NavigationContainer>
   );
 };
