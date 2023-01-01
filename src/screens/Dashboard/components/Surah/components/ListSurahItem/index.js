7f73370ae669c837ffde6399708a9f70157269cc
Hafif <hafifimammuddyn@gmail.com>
2023-01-01 13:24:29 +0700

    ✨ Slicing surah list item

---
+++ src/screens/Dashboard/components/Surah/components/ListSurahItem/index.js
@@ -0,0 +1,33 @@
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import styles from './styles';
+import { images } from '../../../../../../constants';
+
+const ListSurahItem = ({ onPressListItem, surah }) => {
+  console.log({ surah });
+
+  return (
+    <TouchableOpacity style={styles.listItem} onPress={onPressListItem}>
+      <View style={styles.listItemName}>
+        <ImageBackground
+          source={images.Star}
+          style={styles.listNumberBackground}>
+          <Text style={styles.listNumberText}>{surah.number}</Text>
+        </ImageBackground>
+
+        <View>
+          <Text style={styles.surahName}>{surah.englishName}</Text>
+          <Text style={styles.surahShortDesc}>
+            {surah.revelationType} - {surah.numberOfAyahs} Verses
+          </Text>
+        </View>
+      </View>
+
+      <View>
+        <Text style={styles.surahNameArabic}>{surah.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ListSurahItem;
---
+++ src/screens/Dashboard/components/Surah/components/ListSurahItem/styles.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../../../../../constants';
+
+export default StyleSheet.create({
+  listItem: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: '#BBC4CE59',
+    borderBottomWidth: 1,
+    paddingBottom: 16,
+    marginBottom: 16,
+  },
+  listItemName: {
+    alignItems: 'center',
+    fontFamily: 'Poppins-Regular',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  listNumberBackground: {
+    height: 32,
+    width: 32,
+    marginRight: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    resizeMode: 'cover',
+  },
+  listNumberText: {
+    color: COLORS.black,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  surahName: {
+    color: COLORS.black,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  surahNameArabic: {
+    color: COLORS.primary,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  surahShortDesc: {
+    color: COLORS.black,
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+    fontWeight: '500',
+  },
+});
