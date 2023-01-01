40f40564bd3093ab3b9713a187ba6f45c317b81d
Hafif <hafifimammuddyn@gmail.com>
2023-01-01 20:19:29 +0700

    ♻️ Added dua and salat screen on exports

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,8 @@
-import Dashboard from './Dashboard';
-import Home from './Home';
 import Bookmark from './Bookmark';
+import Dashboard from './Dashboard';
+import Dua from './Dua';
+import Home from './Home';
+import Salat from './Salat';
+import SurahDetail from './SurahDetail';
 
-export { Bookmark, Dashboard, Home };
+export { Bookmark, Dashboard, Dua, Home, Salat, SurahDetail };
