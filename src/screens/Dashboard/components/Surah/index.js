d7e1deaf5dea79dace5b5efd1da0dea9c4725b48
Hafif <hafifimammuddyn@gmail.com>
2023-01-01 13:24:51 +0700

    ♻️ Slicing surah tab

--- src/screens/Dashboard/components/Surah/index.js
+++ src/screens/Dashboard/components/Surah/index.js
@@ -1,11 +1,43 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import ListSurahItem from './components/ListSurahItem';
 import { COLORS } from '../../../../constants';
+import axios from 'axios';
 
 const Surah = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onPressListItem = event => {
+    console.log({ event });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    axios
+      .get('http://api.alquran.cloud/v1/meta')
+      .then(response => {
+        const _data = response?.data?.data;
+        setData(_data?.surahs);
+      })
+      .catch(error => {
+        console.log('FETCH QURAN META FAILED >>> ', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
-      <Text>Surah</Text>
+      {isLoading ? (
+        <ActivityIndicator size="large" color={COLORS.primary} />
+      ) : (
+        data?.references?.map(item => (
+          <ListSurahItem onPressListItem={onPressListItem} surah={item} />
+        ))
+      )}
     </ScrollView>
   );
 };
