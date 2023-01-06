1e9e8add2813220ef84774dfaed9ee69f323b95e
Hafif <hafifimammuddyn@gmail.com>
2023-01-06 23:01:06 +0700

    ✨ Added redux reducers for surah tab on dashboard screen

---
+++ src/screens/Dashboard/redux/reducers.js
@@ -0,0 +1,19 @@
+import { Alert } from 'react-native';
+
+export const setSurahs = (state, action) => {
+  state.surahs = action.payload;
+};
+
+export const surahsRequested = state => {
+  state.loading = true;
+};
+
+export const surahsReceived = (state, action) => {
+  state.surahs = action.payload?.data?.surahs;
+  state.loading = false;
+};
+
+export const surahsRequestFailed = state => {
+  state.loading = false;
+  Alert.alert('Failed to get list of surahs, please try again later.');
+};
