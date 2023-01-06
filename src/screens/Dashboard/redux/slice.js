1cd7bd40394c9cbdb10ae38a8e930d898b30053f
Hafif <hafifimammuddyn@gmail.com>
2023-01-06 23:01:35 +0700

    ✨ Added redux configs for dashboard screen

---
+++ src/screens/Dashboard/redux/slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { dashboardStates } from './states';
+import * as dashboardReducers from './reducers';
+
+export const dashboardSlice = createSlice({
+  name: 'dashboard',
+  initialState: {
+    ...dashboardStates,
+  },
+  reducers: {
+    ...dashboardReducers,
+  },
+});
+
+export default dashboardSlice.reducer;
+
+export const {
+  setSurahs,
+  surahsReceived,
+  surahsRequestFailed,
+  surahsRequested,
+} = dashboardSlice.actions;
