bd83b2699a8f3e9dc60be8812c5f5c0133db5cc8
Hafif <hafifimammuddyn@gmail.com>
2023-01-08 22:35:18 +0700

    ✨ Slicing search ayah modal component

---
+++ src/screens/SurahDetail/components/ModalAyahSearch/index.js
@@ -0,0 +1,47 @@
+import { Modal, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import styles from './styles';
+
+const ModalAyahSearch = ({
+  findAyah,
+  isOpen,
+  onChangeText,
+  onPressReset,
+  onPressSubmit,
+}) => {
+  return (
+    <Modal visible={isOpen} animationType="none" transparent={true}>
+      <View style={styles.modalView}>
+        <View style={styles.modalContent}>
+          <View>
+            <Text style={styles.modalLabel}>
+              Filter ayah by arabic or translation:
+            </Text>
+
+            <TextInput
+              style={styles.modalInput}
+              onChangeText={onChangeText}
+              value={findAyah}
+            />
+          </View>
+
+          <View style={styles.modalFooter}>
+            <TouchableOpacity
+              onPress={onPressReset}
+              style={styles.modalFooterResetButton}>
+              <Text style={styles.modalFooterResetLabel}>Reset</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={onPressSubmit}
+              style={styles.modalFooterSubmitButton}>
+              <Text style={styles.modalFooterSubmitLabel}>Filter</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default ModalAyahSearch;
