b1e3f45ec9ab3ae1596cb975b1df1a0ebaf092c8
Hafif <hafifimammuddyn@gmail.com>
2023-01-08 22:36:35 +0700

    ♻️ Added export loader component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import BottomTabNavigator from './BottomTabNavigator';
+import Loader from './Loader';
 import TabItem from './TabItem';
 
-export { BottomTabNavigator, TabItem };
+export { BottomTabNavigator, Loader, TabItem };
