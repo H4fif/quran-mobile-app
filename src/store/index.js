c5a154a2eabe06467c24fdf039b4a0067055a166
Hafif <hafifimammuddyn@gmail.com>
2023-01-23 17:03:25 +0700

    ♻️ Integrate redux toolkit with redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,35 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { configureStore } from '@reduxjs/toolkit';
-import api from './middleware/api';
-import dashboardReducers from '../screens/Dashboard/redux/slice';
-import surahDetailReducers from '../screens/SurahDetail/redux/slice';
 
-export default configureStore({
-  reducer: {
-    dashboard: dashboardReducers,
-    surahDetail: surahDetailReducers,
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api),
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer as _persistReducer,
+  persistStore as _persistStore,
+} from 'redux-persist';
+
+import api from './middleware/api';
+import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistReducer = _persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(api),
 });
+
+export const persistor = _persistStore(store);
