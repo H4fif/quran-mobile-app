8a5d1392740a1a5d6f4f927810d88beb7afd2c4c
Hafif <hafifimammuddyn@gmail.com>
2023-01-23 17:03:49 +0700

    ♻️ implement redux persist on main entry file

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Router from './src/router';
 import { Provider } from 'react-redux';
-import store from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { Loader } from './src/components';
+import Router from './src/router';
+import { persistor, store } from './src/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Router />
-      </NavigationContainer>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <NavigationContainer>
+          <Router />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
