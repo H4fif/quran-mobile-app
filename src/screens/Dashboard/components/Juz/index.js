a3640db3cd00b65fa8b9698db7eff3fc166a67fd
Hafif <hafifimammuddyn@gmail.com>
2023-01-29 23:01:26 +0700

    ♻️ Update list of juzs: refactor using redux, added juz searches by surah name, surah english translation name, revelation type, surah number, total verses and juz number

--- src/screens/Dashboard/components/Juz/index.js
+++ src/screens/Dashboard/components/Juz/index.js
@@ -1,56 +1,83 @@
 import {
   ActivityIndicator,
-  Alert,
   FlatList,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import React, { useCallback, useEffect, useState } from 'react';
-import { COLORS, api } from '../../../../constants';
+
+import React from 'react';
+import { COLORS } from '../../../../constants';
 import ListJuzItem from './components/ListJuzItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadSurahs } from '../../redux/api';
+import { useMemo } from 'react';
 
 const Juz = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(data);
+  const dispatch = useDispatch();
 
-  const fetchData = useCallback(() => {
-    setIsLoading(true);
+  const { findSurah, loading, juzs } =
+    useSelector(state => state.dashboard) || {};
 
-    api
-      .get('meta')
-      .then(response => {
-        const _data = response?.data?.data;
-        setData(_data);
-      })
-      .catch(error => {
-        console.log('FETCH QURAN META FAILED >>> ', error);
-        Alert.alert('Failed to get list of surah(s), please try again later');
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  const dataMemo = useMemo(() => {
+    if (!findSurah?.length) {
+      return juzs;
+    } else {
+      return {
+        ...juzs,
+        references: juzs.references.filter(juz => {
+          if (isNaN(Number(findSurah))) {
+            const keyword = findSurah.toLowerCase();
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+            const engNameAlike =
+              juz.englishName.toLowerCase().search(keyword) > -1;
+
+            const engNameTranslationAlike =
+              juz.englishNameTranslation.toLowerCase().search(keyword) > -1;
+
+            const typeAlike =
+              juz.revelationType.toLowerCase().search(keyword) > -1;
+
+            const nameAlike = juz.name.toLowerCase().search(keyword) > -1;
+
+            return (
+              engNameAlike || typeAlike || nameAlike || engNameTranslationAlike
+            );
+          } else {
+            const keyword = Number(findSurah);
+            const juzNumberAlike = juz.number === keyword;
+            const juzAyahAlike = juz.ayah === keyword;
+            const surahNumberAlike = juz.surah === keyword;
+            const verseTotalAlike = juz.numberOfAyahs === keyword;
+
+            return (
+              juzNumberAlike ||
+              verseTotalAlike ||
+              juzAyahAlike ||
+              surahNumberAlike
+            );
+          }
+        }),
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [findSurah]);
 
   return (
     <View style={styles.container}>
-      {isLoading ? (
+      {loading ? (
         <ActivityIndicator size="large" color={COLORS.primary} />
       ) : (
         <FlatList
-          data={data?.juzs?.references}
+          data={dataMemo?.references}
           ListEmptyComponent={
-            <Text style={styles.labelEmpty}>No data to display</Text>
+            <Text style={styles.labelEmpty}>
+              {!findSurah?.length ? 'No match' : 'No data to display'}
+            </Text>
           }
-          onRefresh={() => fetchData()}
-          renderItem={({ item, index }) => (
-            <ListJuzItem data={data} juz={item} index={index} />
-          )}
-          refreshing={isLoading}
+          onRefresh={() => dispatch(loadSurahs())}
+          renderItem={({ item }) => <ListJuzItem data={dataMemo} juz={item} />}
+          refreshing={loading}
           showsVerticalScrollIndicator={false}
         />
       )}
