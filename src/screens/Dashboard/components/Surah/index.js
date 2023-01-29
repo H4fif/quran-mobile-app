311368f4929dd4c37e96a36db4ad4a861aa6fd4f
Hafif <hafifimammuddyn@gmail.com>
2023-01-29 22:57:29 +0700

    ♻️ Update surah list: added search keywords using surah name in arabic, surah name in english translation, revelation type, surah number and total verses

--- src/screens/Dashboard/components/Surah/index.js
+++ src/screens/Dashboard/components/Surah/index.js
@@ -5,11 +5,13 @@ import {
   Text,
   View,
 } from 'react-native';
-import React, { useCallback, useEffect } from 'react';
+
+import React, { useCallback, useEffect, useMemo } from 'react';
 import ListSurahItem from './components/ListSurahItem';
 import { COLORS } from '../../../../constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadSurahs } from '../../redux/api';
+import { setFindSurah } from '../../redux/slice';
 
 const Surah = ({ navigation }) => {
   const dispatch = useDispatch();
@@ -34,6 +36,47 @@ const Surah = ({ navigation }) => {
     [onPressListItem],
   );
 
+  const onRefresh = useCallback(() => {
+    dispatch(setFindSurah(''));
+    dispatch(loadSurahs());
+  }, [dispatch]);
+
+  const dataMemo = useMemo(
+    () =>
+      !findSurah?.length
+        ? surahs?.references
+        : surahs?.references?.filter(surah => {
+            if (isNaN(Number(findSurah))) {
+              const keyword = findSurah.toLowerCase();
+
+              const engNameAlike =
+                surah.englishName.toLowerCase().search(keyword) > -1;
+
+              const engNameTranslationAlike =
+                surah.englishNameTranslation.toLowerCase().search(keyword) > -1;
+
+              const typeAlike =
+                surah.revelationType.toLowerCase().search(keyword) > -1;
+
+              const nameAlike = surah.name.toLowerCase().search(keyword) > -1;
+
+              return (
+                engNameAlike ||
+                typeAlike ||
+                nameAlike ||
+                engNameTranslationAlike
+              );
+            } else {
+              const keyword = Number(findSurah);
+              const surahNumberAlike = surah.number === keyword;
+              const verseTotalAlike = surah.numberOfAyahs === keyword;
+              return surahNumberAlike || verseTotalAlike;
+            }
+          }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [findSurah],
+  );
+
   useEffect(() => {
     dispatch(loadSurahs());
   }, [dispatch]);
@@ -44,24 +87,17 @@ const Surah = ({ navigation }) => {
         <ActivityIndicator size="large" color={COLORS.primary} />
       ) : (
         <FlatList
-          data={
-            !findSurah?.length
-              ? surahs?.references
-              : surahs?.references?.filter(
-                  surah =>
-                    surah.englishName
-                      .toLowerCase()
-                      .search(findSurah.toLowerCase()) > -1,
-                )
-          }
+          data={dataMemo}
           renderItem={renderItem}
           showsVerticalScrollIndicator={false}
           style={styles.container}
           ListEmptyComponent={
-            <Text style={styles.labelEmpty}>No data to display</Text>
+            <Text style={styles.labelEmpty}>
+              {findSurah?.length > 0 ? 'No match' : 'No data to display'}
+            </Text>
           }
-          refreshing={loading || false}
-          onRefresh={() => dispatch(loadSurahs())}
+          refreshing={loading}
+          onRefresh={onRefresh}
         />
       )}
     </View>
